5412007aaf18cef14435d1a084afa9090aad4250
Shahanawaz <shahanawazkhan579@gmail.com>
2023-09-25 10:14:21 +0530

    Linked Sanity with frontend & completed HeroBanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,9 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { urlFor } from "../lib/client";
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div>
-      <h1>HeroBanner</h1>
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={urlFor(heroBanner.image)} alt="headphones" layout="fill" className="hero-banner-image" />
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
