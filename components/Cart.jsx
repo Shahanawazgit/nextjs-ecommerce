8e1dec7089b9731e9a73cfbfacbe78fcf0e123f2
Shahanawaz <shahanawazkhan579@gmail.com>
2023-09-26 14:52:03 +0530

    Completed Cart with usecontext

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,9 +1,84 @@
-import React from "react";
+import Link from "next/link";
+import React, { useRef } from "react";
+import toast from "react-hot-toast";
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShoppingCart } from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { setShowCart, cartItems, totalPrice, totalQuantities, toggleCartItemQuantity, onRemove } = useStateContext();
+
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button type="button" className="cart-heading" onClick={() => setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShoppingCart size={150} />
+            <h3>Your shopping cart is empty!</h3>
+            <Link href="/">
+              <button type="button" className="btn" onClick={() => setShowCart(false)}>
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={urlFor(item?.image[0])} alt="product-image" className="cart-product-image" />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>${item.price}</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span className="minus" onClick={() => toggleCartItemQuantity(item._id, "dec")}>
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num" onClick={() => {}}>
+                          {item.quantity}
+                        </span>
+                        <span className="plus" onClick={() => toggleCartItemQuantity(item._id, "inc")}>
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <button type="button" className="remove-item" onClick={() => onRemove(item)}>
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal: </h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick={() => {}}>
+                Pay with Stripe
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
 
@@ -6,8 +7,16 @@ const Footer = () => {
     <div className="footer-container">
       <p>&copy; 2023 SP Headphoners All rights reserved!</p>
       <p className="icons">
-        <AiFillLinkedin />
-        <AiOutlineGithub />
+        <Link href="https://linkedin.com/in/shahanawaz-pathan">
+          <a target="_blank" rel="noopener noreferrer">
+            <AiFillLinkedin />
+          </a>
+        </Link>
+        <Link href="https://github.com/Shahanawazgit">
+          <a target="_blank" rel="noopener noreferrer">
+            <AiOutlineGithub />
+          </a>
+        </Link>
       </p>
     </div>
   );
